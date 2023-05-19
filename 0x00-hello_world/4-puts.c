16c1094706a1fcf2cb43997efa4a1039c8b2be87
DikeIsaac <dikeisaac2020@gmail.com>
2023-05-19 04:43:21 +0100

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 sucessful
+ */
+int main(void)
+{
+	char *text = "Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
