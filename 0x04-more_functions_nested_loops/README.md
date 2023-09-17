49b07d2b6e2b91198874dde914de7a3263c4eda4
DikeIsaac <99301933+DikeIsaac@users.noreply.github.com>
2023-09-17 15:36:50 +0100

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,17 @@
+TASK 1
+Write a function that checks for uppercase character.
+
+Prototype: int _isupper(int c);
+Returns 1 if c is uppercase
+Returns 0 otherwise
+
+
+TASK 2
+TASK 3
+TASK 4
+TASK 5
+TASK 6
+TASK 7
+TASK 8 
+TASK 9
+TASK 10
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to standard output
+ * @c: The character to display
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and no error is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int _isupper(int c)
+{
+return 0;
+}
+
