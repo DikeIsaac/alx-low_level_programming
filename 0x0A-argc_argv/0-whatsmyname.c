bf0fbe5029e0f2b296dea6c553837fc2981028d7
DikeIsaac <dikeisaac2020@gmail.com>
2023-11-07 17:43:07 +0100

    modified 0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,12 @@
  *
  * Return: Always o.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int  argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 1)
+	{
+		printf("%s\n", argv[0]);
+	}
 
 	return (0);
 }
